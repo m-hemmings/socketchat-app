b646e3550b931c4ab51e5df40029aa5f8b06e88f
Matt Hemmings <matt.hemmings@engagelively.com>
2023-04-27 10:36:01 -0700

    Factored server code into functionally grouped files for easy management

--- server/server.js
+++ server/server.js
@@ -1,47 +1,10 @@
 const http = require('http');
 const socketio = require('socket.io');
-const mongoose = require('mongoose');
-const { augmentedLog, augmentedError, augmentedDebug } = require('./utilities/logging');
-
-console.log = augmentedLog();
-console.error = augmentedError();
-console.debug = augmentedDebug();
+const { connectToDB } = require('./utilities/db');
+const configureSocket = require('./utilities/socket');
 
 // Connect to MongoDB database
-(async function() {
-  try {
-    await mongoose.connect('mongodb://mongo/chat-room', { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('Connected to MongoDB database');
-  } catch (err) {
-    console.error('Failed to connect to MongoDB database:', err);
-    process.exit(1);
-  }
-})();
-
-const MessageSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  content: {
-    text: { type: String, required: true },
-    timestamp: { type: Date, default: Date.now }
-  }
-});
-
-class Message {
-  constructor(username, content) {
-    this.username = username;
-    this.content = content;
-  }
-
-  static fromJSON(json) {
-    return new Message(json.username, json.content);
-  }
-}
-
-Message.schema = MessageSchema;
-const MessageModel = mongoose.model('Message', MessageSchema);
+connectToDB();
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -55,45 +18,8 @@ const io = socketio(server, {
   }
 });
 
-io.on('connection', async (socket) => {
-  console.log('A user connected');
-
-  // Retrieve and send previous messages
-  try {
-    const messages = await MessageModel.find().sort({ timestamp: -1 }).limit(10).exec();
-    socket.emit('previous-messages', messages.reverse().map(m => Message.fromJSON(m.toJSON())));
-  } catch (err) {
-    console.error(err);
-  }
-
-  // Handle incoming messages
-  socket.on('message', async (message) => {
-    console.log(`Received message: ${JSON.stringify(message)}`);
-
-    // Broadcast the content of the message object to all connected clients
-    io.emit('message', message);
-
-    // Save message to database
-    const newMessage = new MessageModel({ 
-      username: message.username,
-      content: {
-        text: message.content.text,
-        timestamp: new Date().getTime()
-      }
-    });
-
-    try {
-      await newMessage.save();
-    } catch (err) {
-      console.error(err);
-    }
-  });
-
-  // Handle disconnections
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
-});
+// Configure socket
+configureSocket(io);
 
 server.listen(3123, () => {
   console.log('Server running at http://localhost:3123/');
---
+++ server/utilities/socket.js
@@ -0,0 +1,43 @@
+const { Message, MessageModel } = require('./message');
+
+module.exports = function(io) {
+  io.on('connection', async (socket) => {
+    console.log('A user connected');
+
+    // Retrieve and send previous messages
+    try {
+      const messages = await MessageModel.find().sort({ timestamp: -1 }).limit(10).exec();
+      socket.emit('previous-messages', messages.reverse().map(m => Message.fromJSON(m.toJSON())));
+    } catch (err) {
+      console.error(err);
+    }
+
+    // Handle incoming messages
+    socket.on('message', async (message) => {
+      console.log(`Received message: ${JSON.stringify(message)}`);
+
+      // Broadcast the content of the message object to all connected clients
+      io.emit('message', message);
+
+      // Save message to database
+      const newMessage = new MessageModel({ 
+        username: message.username,
+        content: {
+          text: message.content.text,
+          timestamp: new Date().getTime()
+        }
+      });
+
+      try {
+        await newMessage.save();
+      } catch (err) {
+        console.error(err);
+      }
+    });
+
+    // Handle disconnections
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
+    });
+  });
+};
